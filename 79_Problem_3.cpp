50c38e08d3d0347431d1cbf7a4c610e24c525221
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-08 10:52:33 -0400

    Create 79_Problem_3.cpp

---
+++ 79_Problem_3.cpp
@@ -0,0 +1,26 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+double largestSumOfAverages (vector<int>& a, int k) {
+
+   int n = a.size ();
+   vector<double> c = { 0.0 };
+   partial_sum (a.begin (), a.end (), back_inserter (c));
+
+   vector<vector<double>> dp (n + 1, vector<double> (k + 1, -1));
+   dp[0][0] = 0;
+
+   for (int i = 1; i <= n; i++)
+      for (int d = 1; d <= k; d++)
+         for (int j = 0; j < i; j++)
+            if (dp[j][d - 1] != -1)
+               dp[i][d] = max (dp[i][d], dp[j][d - 1] + (c[i] - c[j]) / (i - j));
+
+   return dp[n][k];
+}
