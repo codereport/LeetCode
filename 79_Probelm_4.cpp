2be42283dd38c0aa8004d8d6daa4b80386ce70c6
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-12 00:47:49 -0400

    Create 79_Probelm_4.cpp

---
+++ 79_Probelm_4.cpp
@@ -0,0 +1,44 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+int numBusesToDestination (vector<vector<int>>& routes, int S, int T) 
+{
+   unordered_map<int, unordered_set<int>> stop_routes;
+   
+   for (int i = 0; i < routes.size (); ++i) 
+      for (int j : routes[i]) stop_routes[j].insert (i);
+
+   queue<pair<int, int>> to_visit; 
+   to_visit.push ({ S, 0 });
+   unordered_set<int> stops_visited = { S };
+
+   while (!to_visit.empty ()) 
+   {
+      int stop  = to_visit.front ().first;
+      int bus_n = to_visit.front ().second;
+      
+      if (stop == T) return bus_n;
+      to_visit.pop ();
+      
+      for (const auto& route : stop_routes[stop]) {
+         for (const auto& next_stop : routes[route]) {
+            if (!stops_visited.count (next_stop)) {
+               stops_visited.insert (next_stop);
+               to_visit.push ( { next_stop, bus_n + 1 } );
+            }
+         }
+         
+         routes[route].clear ();
+      }
+   }
+
+   return -1;
+}
