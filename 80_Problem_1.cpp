0225ea18c66a01d8e18a0bc08d49ebba8fe7ec75
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-15 12:27:36 -0400

    Create 80_Problem_1.cpp

---
+++ 80_Problem_1.cpp
@@ -0,0 +1,27 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+string mostCommonWord (string p, vector<string>& banned) 
+{
+   unordered_set<string> b (banned.begin (), banned.end ());
+   unordered_map<string, int> m;
+
+   for (auto& c : p) c = isalpha (c) ? tolower (c) : ' ';
+
+   pair<string, int> ans = { "", 0 };
+   istringstream iss (p);
+   string w;
+
+   while (iss >> w)
+      if (b.count (w) == 0 && ++m[w] > ans.second) ans = { w, m[w] };
+
+   return ans.first;
+}
