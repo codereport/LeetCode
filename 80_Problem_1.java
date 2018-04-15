89909da34867b2ddcf49151eacc690a3287cb4be
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-15 13:22:26 -0400

    Create 80_Problem_1.java

---
+++ 80_Problem_1.java
@@ -0,0 +1,26 @@
+// code_report Solution
+// 
+
+import java.util.*;
+import java.util.Arrays;
+ 
+public String mostCommonWord(String p, String[] banned) 
+{
+    Set<String> b = new HashSet<>(Arrays.asList (banned));
+    Map<String, Integer> m = new HashMap<> ();
+    String[] words = p.replaceAll("[^a-zA-z ]" , "").toLowerCase ().split ("\\s+");
+    String ans  = ""; 
+    Integer max = 0;
+
+    for (String w : words) {
+        if (!b.contains (w)) {
+            m.put (w, m.getOrDefault (w, 0) + 1);
+            if (m.get (w) > max) {
+                ans = w; 
+                max = m.get (w);
+            }
+        }
+    }
+    
+    return ans;
+}
