86882312b68d37a57ae52ee58df998a921fc9835
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-15 12:51:32 -0400

    Create 80_Problem_1.py

---
+++ 80_Problem_1.py
@@ -0,0 +1,13 @@
+// code_report Solution
+// 
+
+    def mostCommonWord(self, p, banned):
+
+       words = re.sub(r'[^a-zA-Z]', ' ', p).lower().split()
+       m = collections.defaultdict(int)
+       ans = (0, "")
+       for word in words:
+          if word not in banned:
+                m[word] += 1
+                ans = max (ans, (m[word], word))
+       return ans[1]       
