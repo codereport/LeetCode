7ae47831d6dc5b65adaf2b5f8e5d14756e4bb96a
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-05-27 20:31:47 -0400

    Create 86_Problem_2.java

---
+++ 86_Problem_2.java
@@ -0,0 +1,23 @@
+// code_report solution
+// 
+
+public boolean canVisitAllRooms (List<List<Integer>> rooms) 
+{
+    Stack<Integer> todo   = new Stack<> (); 
+    todo.add (0);
+    HashSet<Integer> done = new HashSet<Integer> (); 
+    done.add (0);
+    
+    while (!todo.isEmpty ()) 
+    {
+        int i = todo.pop ();
+        for (int j : rooms.get (i)) {
+            if (!done.contains (j)) {
+                todo.add (j);
+                done.add (j);
+            }
+        }
+    }
+
+    return rooms.size() == done.size();
+}
