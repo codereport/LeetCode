65a580e245558c52cc538e854a421057877aca85
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-05-27 20:33:31 -0400

    Create 86_Problem_2.cpp

---
+++ 86_Problem_2.cpp
@@ -0,0 +1,29 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+bool canVisitAllRooms (const vector<vector<int>>& rooms) 
+{
+   stack<int> todo; 
+   todo.push (0);
+   unordered_set<int> done = { 0 };
+   
+   while (!todo.empty ()) 
+   {
+      auto top = todo.top (); 
+      todo.pop ();
+      for (auto room : rooms[top]) {
+         if (done.find (room) == done.end ()) {
+            todo.push (room);
+            done.insert (room);
+         }
+      }
+   }
+
+   return rooms.size () == done.size ();
+}
