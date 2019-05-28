324f579ca79ff9de416252e8a13d07e32f3bec01
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-05-27 23:02:33 -0700

    Update 138_Problem_P1.py

--- 138_Problem_P1.py
+++ 138_Problem_P1.py
@@ -1,6 +1,6 @@
 # code_report Solution
-# Video Link:
-# Problem Link:
+# Video Link: https://leetcode.com/contest/weekly-contest-138/problems/height-checker/
+# Problem Link: https://youtu.be/i-KWz4ZHwFk
 
 def heightChecker(self, h: List[int]) -> int:
         return sum(a != b for a, b in zip(h, sorted(h)))
