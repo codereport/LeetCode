e7834b4707fd19516c9903cbb299a2c79e2523ee
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-04 00:34:08 -0700

    Update 154_Problem_1.scala

--- 154_Problem_1.scala
+++ 154_Problem_1.scala
@@ -1,4 +1,5 @@
 // code_report
+// Problem Link: https://leetcode.com/problems/maximum-number-of-balloons/
 
 def maxNumberOfBalloons(text: String): Int = {
     return "balon".toList.zip(List(1,1,2,2,1)) 
