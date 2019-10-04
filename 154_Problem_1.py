e279c351b5881eb180c70336d042c98fe60630c6
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-04 00:33:49 -0700

    Update 154_Problem_1.py

--- 154_Problem_1.py
+++ 154_Problem_1.py
@@ -1,4 +1,5 @@
-// code_report
+# code_report
+# Problem Link: https://leetcode.com/problems/maximum-number-of-balloons/
 
 def maxNumberOfBalloons(self, t: str) -> int:
     return min(t.count(a) // b for a, b in zip('balon', [1,1,2,2,1]))
