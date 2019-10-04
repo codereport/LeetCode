5679e8337d247c94dfd6c51f3cbfdbef8d7c00f1
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-04 00:32:44 -0700

    Create 155_Problem_1.py

---
+++ 155_Problem_1.py
@@ -0,0 +1,7 @@
+# code_report
+# Problem Link: https://leetcode.com/contest/weekly-contest-155/problems/minimum-absolute-difference/
+
+def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+    arr.sort()
+    min_diff = min(b - a for a, b in zip (arr, arr[1:]))
+    return [[a,b] for a, b in zip (arr, arr[1:]) if b - a == min_diff]
