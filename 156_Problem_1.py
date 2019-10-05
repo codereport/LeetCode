c47ddf3e7cb846ffeb4ad97ae8e80ed6140cd738
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-05 01:02:42 -0700

    Create 156_Problem_1.py

---
+++ 156_Problem_1.py
@@ -0,0 +1,11 @@
+// code_report Solution
+// Problem Link: https://leetcode.com/contest/weekly-contest-156/problems/unique-number-of-occurrences/
+
+def group(l): 
+    return [list(x) for _, x in itertools.groupby(l)]
+
+def freq(l):  
+    return [len(x) for x in group(sorted(l))]
+
+def uniqueOccurrences(self, arr: List[int]) -> bool:
+    return max(freq(freq(arr))) <= 1
