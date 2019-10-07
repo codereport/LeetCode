b9d8938ef332d21e58fc2e35e14111afead59d2e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-06 22:24:47 -0700

    Update 156_Problem_1.py

--- 156_Problem_1.py
+++ 156_Problem_1.py
@@ -1,5 +1,5 @@
-// code_report Solution
-// Problem Link: https://leetcode.com/contest/weekly-contest-156/problems/unique-number-of-occurrences/
+# code_report Solution
+# Problem Link: https://leetcode.com/contest/weekly-contest-156/problems/unique-number-of-occurrences/
 
 def group(l): 
     return [list(x) for _, x in itertools.groupby(l)]
