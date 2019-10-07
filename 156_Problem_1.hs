a93a29ae8126f08cb888f253caf28b32704788d4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-06 22:32:18 -0700

    Update 156_Problem_1.hs

--- 156_Problem_1.hs
+++ 156_Problem_1.hs
@@ -1,5 +1,5 @@
-// code_report Solution
-// Problem Link: https://leetcode.com/contest/weekly-contest-156/problems/unique-number-of-occurrences/
+-- code_report Solution
+-- Problem Link: https://leetcode.com/contest/weekly-contest-156/problems/unique-number-of-occurrences/
 
 import Data.List
 
