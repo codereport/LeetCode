03df8c61012122e41ea1da478f891ce9b671a361
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-10-18 13:31:32 -0700

    Update 119_Problem_1.cpp

--- 119_Problem_1.cpp
+++ 119_Problem_1.cpp
@@ -1,6 +1,18 @@
 // code_report Solution
-// Video Solution: https://youtu.be/5FNh2HKYSjQ
-// Problem Link: https://leetcode.com/contest/weekly-contest-119/problems/k-closest-points-to-origin/
+// Video Solution:          https://youtu.be/5FNh2HKYSjQ
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-119/problems/k-closest-points-to-origin/
+// Problem Link (Practice): https://leetcode.com/problems/k-closest-points-to-origin/
+
+// More efficient solution
+
+vector<vector<int>> kClosest(vector<vector<int>>& p, int K) {
+   auto dist = [ ](auto a, auto b) { return sqrt(a * a + b * b); };
+   auto comp = [&](auto a, auto b) { return dist(a[0], a[1]) < dist(b[0], b[1]); };
+   nth_element(begin(p), begin(p) + K, end(p), comp);
+   return vector<vector<int>>(begin(p), begin(p) + K);
+}
+
+// Less efficient solution
 
 vector<vector<int>> kClosest(vector<vector<int>>& p, int K) {
    auto dist = [ ](auto a, auto b) { return sqrt(a * a + b * b); };
