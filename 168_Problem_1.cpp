eeaad5079432190f5c70c2ac4f3b2e0878885e5b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-06 23:14:54 -0800

    Update 168_Problem_1.cpp

--- 168_Problem_1.cpp
+++ 168_Problem_1.cpp
@@ -1,6 +1,6 @@
-# code_report Solution
-# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
-# Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
+// Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
 // C++11 Solution
 
