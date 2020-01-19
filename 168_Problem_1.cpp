7cfe4ebe22a2153434d17190fd6fe8e6d8f2e768
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-19 13:03:12 -0500

    Update 168_Problem_1.cpp

--- 168_Problem_1.cpp
+++ 168_Problem_1.cpp
@@ -2,6 +2,16 @@
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 // Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
+// C++11 Solution (bad)
+
+int findNumbers(vector<int>& nums) {
+    int res = 0; 
+    for (auto e : nums)
+        if (to_string(e).size() % 2 == 0)
+            res++;
+    return res;
+}
+
 // C++11 Solution
 
 int findNumbers(vector<int>& nums) {
