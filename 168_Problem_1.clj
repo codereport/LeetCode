2a601895625c3f9c72e14a58a0c89880fdb4981c
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-19 08:26:57 -0500

    Update 168_Problem_1.clj

--- 168_Problem_1.clj
+++ 168_Problem_1.clj
@@ -2,11 +2,8 @@
 ; Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 ; Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
-(defn even-digits? [num] (->> num
-                              str
-                              count
-                              even?))
-
-(defn find-numbers [nums] (->> nums 
-                               (filter even-digits?)
-                               (reduce +)))
+(defn find-numbers [nums] (->> nums
+                               (map str)
+                               (map count)
+                               (filter even?)
+                               count))
