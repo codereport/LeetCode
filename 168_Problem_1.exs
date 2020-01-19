4f07de0f6f95effcbcd8e875b2598d5a526dc38f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-19 09:24:16 -0500

    Create 168_Problem_1.exs

---
+++ 168_Problem_1.exs
@@ -0,0 +1,15 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
+# Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+import Integer, only: [is_even: 1]
+
+defmodule LeetCode do
+  def findNumbers(collection) do
+    collection
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.map(&String.length/1)
+    |> Enum.filter(&is_even/1)
+    |> Enum.count()
+  end
+end
