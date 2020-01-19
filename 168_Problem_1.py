5ddaea571eed32169bf7f5e5d6a7af094f99c2ff
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-19 12:59:43 -0500

    Update 168_Problem_1.py

--- 168_Problem_1.py
+++ 168_Problem_1.py
@@ -2,5 +2,24 @@
 # Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 # Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
+# Solution 1 (bad)
+
+def findNumbers(self, nums: List[int]) -> int:
+    res = 0
+    for num in nums:
+        if (len(str(num)) % 2 == 0):
+            res += 1
+    return res
+
+# Solution 2 (good - idiomatic)
+
 def findNumbers(self, nums: List[int]) -> int:
         return len([i for i in nums if len(str(i)) % 2 == 0])
+
+# Solution 3 (bad - non-idiomatic)
+
+def findNumbers(self, nums: List[int]) -> int:
+    s = map(str, nums)
+    i = map(len, s)
+    j = filter(lambda e: e % 2 == 0, i)
+    return len(list(j))
