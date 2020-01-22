dfe3d405f7e888b351b1c15564468f1369e8ea21
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-22 00:48:46 -0500

    Update 168_Problem_1.py

--- 168_Problem_1.py
+++ 168_Problem_1.py
@@ -1,7 +1,8 @@
 # code_report Solution
 # Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 # Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-
+# Video Solution:          https://youtu.be/gZLZPrgqw2A
+        
 # Solution 1 (bad)
 
 def findNumbers(self, nums: List[int]) -> int:
