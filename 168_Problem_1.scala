b044541e298dcb2119b56d8f834c64af8e4f199c
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-24 00:41:47 -0500

    Update 168_Problem_1.scala

--- 168_Problem_1.scala
+++ 168_Problem_1.scala
@@ -1,6 +1,7 @@
 // code_report Solution
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 // Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// Video Solution:          https://youtu.be/gZLZPrgqw2A
 
 def findNumbers(nums: Array[Int]): Int = {
     nums.map(_.toString.length)
