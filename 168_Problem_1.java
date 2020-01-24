332b4af0c66d22818a8de6c04d9f263a0930f999
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-23 21:49:26 -0500

    Update 168_Problem_1.java

--- 168_Problem_1.java
+++ 168_Problem_1.java
@@ -1,6 +1,7 @@
 // code_report Solution
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 // Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// Video Solution:          https://youtu.be/gZLZPrgqw2A
 
 public int findNumbers(int[] nums) {
      return (int) Arrays.stream(nums)
