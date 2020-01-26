e449c49671da6daa7ceca55efaadbf40c9abe800
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-01-26 00:37:58 -0500

    Update 168_Problem_1.rs

--- 168_Problem_1.rs
+++ 168_Problem_1.rs
@@ -1,6 +1,7 @@
 // code_report Solution
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 // Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// Video Solution:          https://youtu.be/gZLZPrgqw2A
 
 pub fn find_numbers(nums: Vec<i32>) -> i32 {
     return nums.iter()
