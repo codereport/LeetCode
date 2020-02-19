b2c7f512abbcba78928a9512f0079a770d5017eb
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-19 00:17:02 -0500

    Create 0176_Problem_1.cpp

---
+++ 0176_Problem_1.cpp
@@ -0,0 +1,27 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+// Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+// Godbolt Link:            https://godbolt.org/z/sx6b9K
+
+#include <vector>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/join.hpp>
+
+namespace rv = ranges::views;
+
+namespace hs {
+    auto length() {
+        return ranges::make_pipeable([](auto&& rng) {
+            return std::distance(
+                ranges::begin(rng), 
+                ranges::end(rng));
+        });
+    }
+}
+
+auto countNegatives(std::vector<std::vector<int>> const& grid) -> int {    
+    return grid 
+        | rv::join
+        | rv::filter([](auto e) { return e < 0; })
+        | hs::length();
+}
