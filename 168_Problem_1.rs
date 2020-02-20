25c80f07f26735ee807c77107bc2ef739fad9adb
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-20 02:06:46 -0500

    Update 168_Problem_1.rs

--- 168_Problem_1.rs
+++ 168_Problem_1.rs
@@ -2,6 +2,7 @@
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-168/problems/find-numbers-with-even-number-of-digits/
 // Problem Link (Practice): https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 // Video Solution:          https://youtu.be/gZLZPrgqw2A
+// Rust Playground Link:    https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=c9c11e0d5b5c03e931bfeda35e6114cc
 
 pub fn find_numbers(nums: Vec<i32>) -> i32 {
     return nums.iter()
