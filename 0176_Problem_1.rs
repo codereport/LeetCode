0c4bd9024bac555388d4cbb8cc534c49841bd3f6
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-20 02:23:57 -0500

    Create 0176_Problem_1.rs

---
+++ 0176_Problem_1.rs
@@ -0,0 +1,11 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+// Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+// Rust Playground Link:    https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=3641de477438f32ad2faa2ef544aaa15
+
+pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+    return grid.iter()
+               .flatten()
+               .filter(|i| i < &&0i32)
+               .count() as i32;
+}
