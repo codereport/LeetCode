8c859bd8978acb3388c008a5f3090b22abe5e6f4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-21 01:00:06 -0500

    Create 0176_Problem_1.kt

---
+++ 0176_Problem_1.kt
@@ -0,0 +1,9 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+// Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+fun countNegatives(grid: Array<IntArray>): Int {
+    return grid.flatMap { it.asIterable() }
+               .filter { it < 0 }
+               .count();
+}
