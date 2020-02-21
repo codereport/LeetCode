ce7ce565a906d5ae35d3a6c0186f4a7cfcd5dbc2
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-21 11:02:11 -0500

    Create 0176_Problem_1.d

---
+++ 0176_Problem_1.d
@@ -0,0 +1,13 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+// Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+import std.stdio;
+import std.array : join;
+import std.algorithm.searching : count;
+
+int countNegatives(int[][] grid) {
+    return cast(int) grid
+        .join
+        .count!(x => x < 0);
+}
