cef104e30e53fe47e29a24c89222cd05ca659449
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-22 17:56:12 -0500

    Update 0176_Problem_1.hs

--- 0176_Problem_1.hs
+++ 0176_Problem_1.hs
@@ -2,8 +2,8 @@
 -- Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
 -- Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
 
-solve :: [[Int]] -> Int
-solve = length 
-      . filter (==True) 
-      . map (<0) 
-      . concat
+countNegatives :: [[Int]] -> Int
+countNegatives = length 
+               . filter (==True) 
+               . map (<0) 
+               . concat
