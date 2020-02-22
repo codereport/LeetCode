8b97d058484c99e87f3bc034efd7801ea82ce7f4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-22 01:09:56 -0500

    Create 0176_Problem_1.rb

---
+++ 0176_Problem_1.rb
@@ -0,0 +1,7 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+# Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+def count_negatives(grid)
+    return grid.flatten.count { |x| x < 0 }
+end
