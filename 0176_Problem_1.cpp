dba3c16162202d18edebcf864c5e62a7207ad9ea
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-26 02:12:54 -0500

    Update 0176_Problem_1.cpp

--- 0176_Problem_1.cpp
+++ 0176_Problem_1.cpp
@@ -2,6 +2,7 @@
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
 // Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
 // Godbolt Link:            https://godbolt.org/z/sx6b9K
+// Video Link:              https://youtu.be/pDbDtGn1PXk
 
 #include <vector>
 #include <range/v3/view/filter.hpp>
