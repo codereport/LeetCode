ea526bcc1e9a7d1ca412abc5c40890f1381ed9fe
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-02-28 00:27:30 -0500

    Create 0176_Problem_1.cs

---
+++ 0176_Problem_1.cs
@@ -0,0 +1,9 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+// Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+// Video Link:              https://youtu.be/pDbDtGn1PXk
+
+public int CountNegatives(int[][] grid) {
+    return grid.SelectMany(x => x)
+               .Count(x => x < 0);
+}
