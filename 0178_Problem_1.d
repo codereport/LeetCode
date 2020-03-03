8e7e0e524e3613be99bd92de373d92f1d556c9af
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-03 01:48:16 -0500

    Create 0178_Problem_1.d

---
+++ 0178_Problem_1.d
@@ -0,0 +1,16 @@
+
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-178/problems/how-many-numbers-are-smaller-than-the-current-number/
+// Problem Link (Practice): https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+import std.algorithm.setops : cartesianProduct;
+import std.algorithm.iteration : map, sum;
+import std.range : chunks;
+
+auto smallThanCurrentNumber(int[] nums) {
+    return nums
+        .cartesianProduct(nums)
+        .map!(x => x[0] > x[1] ? 1 : 0)
+    	.chunks(nums.length)
+        .map!(x => x.sum);
+}
