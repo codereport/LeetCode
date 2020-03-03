2ef575057a13e9c2edde76b5d5f20e2c9aaed4b3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-03 00:32:46 -0500

    Create 0178_Problem_1.fs

---
+++ 0178_Problem_1.fs
@@ -0,0 +1,11 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-178/problems/how-many-numbers-are-smaller-than-the-current-number/
+// Problem Link (Practice): https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+
+let countNegatives nums = 
+    nums 
+    |> List.allPairs(nums)
+    |> List.map(fun (a, b) -> if a > b then  1 else 0)
+    |> List.chunkBySize(nums.Length)
+    |> List.map List.sum
+    
