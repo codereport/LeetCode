414c39b613617867210da74438e64f552870e741
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-05 02:59:07 -0500

    Update 0015_Problem_1.hs

--- 0015_Problem_1.hs
+++ 0015_Problem_1.hs
@@ -2,9 +2,16 @@
 -- Problem Link (Contest):  https://leetcode.com/contest/leetcode-weekly-contest-15/problems/max-consecutive-ones/
 -- Problem Link (Practice): https://leetcode.com/problems/max-consecutive-ones/
 
+-- Solution 1
 maxConsecutiveOnes :: [Int] -> Int
 maxConsecutiveOnes = maximum 
                    . (0:)
                    . map length
                    . filter ((1==) . head)
                    . group 
+                   
+-- Solution 2
+maxConsecutiveOnes :: [Int] -> Int
+maxConsecutiveOnes = maximum 
+                   . map sum
+                   . group                    
