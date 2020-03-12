aa0aba8f4afb30996cd692565b20814243d73e24
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-12 01:42:21 -0400

    Update 0014_Problem_1.rb

--- 0014_Problem_1.rb
+++ 0014_Problem_1.rb
@@ -2,6 +2,7 @@
 # Problem Link (Contest):  https://leetcode.com/contest/leetcode-weekly-contest-14/problems/number-complement/
 # Problem Link (Practice): https://leetcode.com/problems/number-complement/
 
+# Solution 1
 def find_complement(num)
     return num.to_s(2)
               .chars
@@ -10,4 +11,13 @@ def find_complement(num)
               .each_with_index 
               .map { |val, i| val * ( 2 ** i) }
               .sum
+end
+
+# Solution 2 (Better)
+def find_complement(num)
+    return num.to_s(2)
+              .chars
+              .map { |x| 1 - x.to_i }
+              .join
+              .to_i(2)
 end
