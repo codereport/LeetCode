dc38a572b0469ffe592b74c73a9c2d43c917a09a
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-12 00:28:00 -0400

    Create 0019_Problem_1.rb

---
+++ 0019_Problem_1.rb
@@ -0,0 +1,7 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/leetcode-weekly-contest-19/problems/base-7/
+# Problem Link (Practice): https://leetcode.com/problems/base-7/
+
+def convert_to_base7(num)
+    return num.to_s(7)
+end
