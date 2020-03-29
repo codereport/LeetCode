0e217a97067bbb90255c73e9cc2d4359b5ae4daa
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-03-29 14:58:16 -0400

    Create 0182_Problem_1.clj

---
+++ 0182_Problem_1.clj
@@ -0,0 +1,12 @@
+; code_report Solution
+; Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-182/problems/find-lucky-integer-in-an-array/
+; Problem Link (Practice): https://leetcode.com/problems/find-lucky-integer-in-an-array/
+
+(defn find-lucky [arr]
+  (->> arr
+       (frequencies)
+       (filter #(= (first %) (second %)))
+       (keys)
+       (concat [-1])
+       (reduce max)))
+       
