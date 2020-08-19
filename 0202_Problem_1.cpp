4c16d8e933434fe3a839652053210bd5db03b7f9
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-08-18 20:34:38 -0400

    Create 0202_Problem_1.cpp

---
+++ 0202_Problem_1.cpp
@@ -0,0 +1,16 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-202/problems/three-consecutive-odds/
+// Problem Link (Practice): https://leetcode.com/problems/three-consecutive-odds/
+
+// Note this problem is very similar to MCO (Max Consecutive Ones)
+
+// Godbold link: https://godbolt.org/z/xWsaor
+
+auto three_consecutive_odds(auto const& arr) -> bool {
+    auto const max_odds = arr
+        | rv::group_by([](auto a, auto b) { return a % 2 == b % 2; })
+        | rv::filter([](auto rng) { return rng[0] % 2 == 1; })
+        | rv::transform(ranges::distance)
+        | hs::maximum();
+    return max_odds >= 3;
+}
