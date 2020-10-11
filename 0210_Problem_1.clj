763871cc051b930c6eb99a611f8e74a70dd09054
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-11 03:32:21 -0400

    Create 0210_Problem_1.clj

---
+++ 0210_Problem_1.clj
@@ -0,0 +1,11 @@
+;; code_report Solution
+;; Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+;; Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+(defn max-depth [s]
+  (->> s 
+       (re-seq #"[()]")
+       (map #(if (= % "(") 1 -1))
+       (reductions +)
+       (apply max)))
+       
