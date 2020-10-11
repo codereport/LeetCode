e058f9e7a72291aebe83499a0907d24a98ac223f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-11 02:15:06 -0400

    Create 0210_Problem_1.rb

---
+++ 0210_Problem_1.rb
@@ -0,0 +1,12 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+# Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+def max_depth(s)
+    return s.tr('^()','')
+            .chars
+            .map{ |c| c == '(' ? 1 : -1 }
+            .inject([]){ |acc, x| acc << acc.last.to_i + x }
+            .push(0)
+            .max
+end
