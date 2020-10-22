6084ad1d8b5da145bb06d9b3088a4f67eb0ad787
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-22 10:01:29 -0400

    Create 0211_Problem_1.cpp

---
+++ 0211_Problem_1.cpp
@@ -0,0 +1,5 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-211/problems/largest-substring-between-two-equal-characters/
+// Problem Link (Practice): https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+
+// Godbolt: https://www.godbolt.org/z/G11oac
