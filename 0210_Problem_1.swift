afe12243cbf16e8e05aa3a0342623dfbfbfc0510
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-24 21:10:23 -0400

    Update 0210_Problem_1.swift

--- 0210_Problem_1.swift
+++ 0210_Problem_1.swift
@@ -6,5 +6,5 @@ func maxDepth(s: String) -> Int {
     return  s.filter { "()".contains($0) }
              .map { $0 == "(" ? 1 : -1 }
              .reduce(into: []) { $0.append(($0.last ?? 0) + $1) }
-             .max()!
+             .max() ?? 0
 }
