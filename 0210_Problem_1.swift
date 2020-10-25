bb06634d86e6492ae49fd0cd6b2f01f4b52e778b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-24 20:36:33 -0400

    Create 0210_Problem_1.swift

---
+++ 0210_Problem_1.swift
@@ -0,0 +1,10 @@
+// code_report Solution
+// Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+// Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+func maxDepth(s: String) -> Int {
+    return  s.filter { "()".contains($0) }
+             .map { $0 == "(" ? 1 : -1 }
+             .reduce(into: []) { $0.append(($0.last ?? 0) + $1) }
+             .max()!
+}
