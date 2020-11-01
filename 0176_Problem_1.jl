4f49e45dc186a8cd5e561790e09da1b0a82f4718
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-31 21:07:33 -0400

    Create 0176_Problem_1.jl

---
+++ 0176_Problem_1.jl
@@ -0,0 +1,15 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-176/problems/count-negative-numbers-in-a-sorted-matrix/
+# Problem Link (Practice): https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+function countnegatives(grid)
+    @pipe grid                |> 
+        vec                   |> 
+        filter(x -> x < 0, _) |> 
+        length
+end
+
+# One-liner
+function countnegatives(grid)
+    @pipe grid |> vec |> filter(x -> x < 0, _) |> length
+end
