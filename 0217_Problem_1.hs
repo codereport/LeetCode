2801cc4bd045a82f079b37c57e681bb8e2286dc0
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-11-29 11:55:27 -0500

    Create 0217_Problem_1.hs

---
+++ 0217_Problem_1.hs
@@ -0,0 +1,6 @@
+-- code_report Solution
+-- Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-217/problems/richest-customer-wealth/
+-- Problem Link (Practice): https://leetcode.com/problems/richest-customer-wealth/
+
+maximumWealth :: [[Int]] -> Int
+maximumWealth = maximum . map sum
