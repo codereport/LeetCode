46ef44805e4b14df9a48d8506fba8aa16dadf0f5
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-11-29 12:03:43 -0500

    Create 0217_Problem_1.clj

---
+++ 0217_Problem_1.clj
@@ -0,0 +1,8 @@
+;; code_report Solution
+;; Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-217/problems/richest-customer-wealth/
+;; Problem Link (Practice): https://leetcode.com/problems/richest-customer-wealth/
+
+(defn maximum-wealth [accounts]
+    (->> accounts 
+         (map #(reduce + %))
+         (reduce max)))
