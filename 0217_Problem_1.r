8de362402385bf09182c2d0e2e49223e4d91439e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-11-30 00:24:38 -0500

    Create 0217_Problem_1.r

---
+++ 0217_Problem_1.r
@@ -0,0 +1,7 @@
+# code_report Solution
+# Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-217/problems/richest-customer-wealth/
+# Problem Link (Practice): https://leetcode.com/problems/richest-customer-wealth/
+
+maximumWealth <- function(accounts) {
+   return(max(rowSums(accounts)))
+}
