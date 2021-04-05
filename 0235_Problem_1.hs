031701fb5b5d332cb3bf3695ae26cdabfd5ce84b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-04-04 22:11:09 -0400

    Create 0235_Problem_1.hs

---
+++ 0235_Problem_1.hs
@@ -0,0 +1,5 @@
+-- code_report Solution
+-- Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-235/problems/truncate-sentence/
+-- Problem Link (Practice): https://leetcode.com/problems/truncate-sentence/
+
+truncateSentence = unwords . take 4 . words
