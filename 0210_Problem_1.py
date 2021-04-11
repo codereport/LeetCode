b98ec1d35f5356d4543683b144c76f4bcd2bf292
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-04-11 11:15:26 -0400

    Update 0210_Problem_1.py

--- 0210_Problem_1.py
+++ 0210_Problem_1.py
@@ -7,11 +7,13 @@
 from itertools import accumulate
 
 def max_depth(s):
-    return max(accumulate((1 if c == "(" else -1 for c in s if c in "()")), default=0)
+    return max(accumulate((1 if c == '(' else -1) for c in s if c
+               in '()'), default=0)
 
 # Solution 2
 
 from itertools import accumulate
 
 def max_depth(s):
-    return max(accumulate(filter(None, map({"(": 1, ")": -1}.get, s))), default=0)
+    return max(accumulate(filter(None, map({'(': 1, ')': -1}.get, s))),
+               default=0)
