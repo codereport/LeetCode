7926cc6a745dd7580cdff4b5e614d9e146e873cc
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-04-11 11:14:48 -0400

    Update 0210_Problem_1.fs

--- 0210_Problem_1.fs
+++ 0210_Problem_1.fs
@@ -2,8 +2,17 @@
 // Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
 // Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 
+// Solution 1
+
 let maxDepth = Seq.map(function | '(' ->  1
                                 | ')' -> -1     
                                 | _   ->  0)
             >> Seq.scan(+) 0
             >> Seq.max
+
+// Solution 2
+
+let maxDepth : string -> int = 
+    Seq.map(function '(' ->  1 | ')' -> -1 | _  ->  0)
+    >> Seq.scan(+) 0
+    >> Seq.max
