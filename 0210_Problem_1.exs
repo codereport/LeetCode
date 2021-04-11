c3cb452c93dd9ee3a2fde391f70d239a4e6fa22d
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-04-10 21:31:18 -0400

    Update 0210_Problem_1.exs

--- 0210_Problem_1.exs
+++ 0210_Problem_1.exs
@@ -1,4 +1,4 @@
- # code_report Solution
+# code_report Solution
 # Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
 # Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 
@@ -11,4 +11,20 @@ defmodule LeetCode do
     |> Enum.scan(&(&1 + &2))
     |> Enum.max()
   end
+end
+
+# Solution 2 (from Jose Valim)
+# https://twitter.com/josevalim/status/1316000995713921025
+
+defmodule LeetCode do
+  def maxDepth(s) do
+    s
+    |> String.to_charlist()
+    |> Enum.map(fn
+      ?( -> 1
+      ?) -> -1
+      _ -> 0
+    end)
+    |> Enum.scan(&+/2)
+    |> Enum.max()
 end
