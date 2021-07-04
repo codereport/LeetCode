e91568e0a647148c9725d70edabc75dc3a12819e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-07-04 17:11:25 -0400

    Create 0248_Problem_1.py

---
+++ 0248_Problem_1.py
@@ -0,0 +1,4 @@
+# Problem Link: https://leetcode.com/contest/weekly-contest-248/problems/build-array-from-permutation/
+
+def buildArray(self, nums: List[int]) -> List[int]:
+    return [nums[i] for i in nums]
