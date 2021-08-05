d202621413cce254c244c0d89895845cce119b92
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-05 19:40:40 -0400

    Update P0053_Max_Subarray_Sum.rs

--- P0053_Max_Subarray_Sum.rs
+++ P0053_Max_Subarray_Sum.rs
@@ -3,10 +3,9 @@
 use core::cmp::max;
 
 pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-    let x = nums.iter()
-                .scan(0, |acc, x| { *acc = max(0, x + *acc); Some(*acc) })
-                .max()
-                .unwrap();
-    if x == 0 { *nums.iter().max().unwrap() } else { x }
+    return nums.iter()
+               .scan(0, |acc, x| { *acc = max(*x, x + *acc); Some(*acc) })
+               .max()
+               .unwrap();
 }
   
