21f44a8fe8859a19b7e05d8049dc3f9e010ac3d6
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-06 02:10:32 -0400

    Update P0053_Max_Subarray_Sum.scala

--- P0053_Max_Subarray_Sum.scala
+++ P0053_Max_Subarray_Sum.scala
@@ -1,4 +1,4 @@
-// Problem Links: https://leetcode.com/problems/maximum-subarray/
+// Problem Link: https://leetcode.com/problems/maximum-subarray/
 
 def maxSubArray(nums: Array[Int]): Int = 
     nums.scan(0)((a, b) => Math.max(b, a + b))
