0c7c4f48b3ea1e34c130f9160131221d537f1366
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-06 01:53:59 -0400

    Create P0053_Max_Subarray_Sum.cpp

---
+++ P0053_Max_Subarray_Sum.cpp
@@ -0,0 +1,10 @@
+// Problem Links: https://leetcode.com/problems/maximum-subarray/
+// Godbolt: https://www.godbolt.org/z/fcqoYhEcK
+
+#include <range/v3/view/partial_sum.hpp>
+
+auto max_sub_array(auto nums) {
+    return std::ranges::max(
+        ranges::views::partial_sum(nums, 
+            [](auto a, auto b) { return std::max(a + b, b); }));
+}
