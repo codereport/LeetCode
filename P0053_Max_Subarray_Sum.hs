ad2190b035ae682784f4584ebee35f7f37eab324
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-06 02:11:20 -0400

    Create P0053_Max_Subarray_Sum.hs

---
+++ P0053_Max_Subarray_Sum.hs
@@ -0,0 +1,12 @@
+-- Problem Link: https://leetcode.com/problems/maximum-subarray/
+
+-- Solution 1
+maxSubArray = maximum 
+            . scanl1 (\a b -> max b (a + b))
+
+-- Solution #2 (APL inspired)
+kite            = flip const
+bfork l b r x y = b (l x y) (r x y) -- bfork = Binary Fork from APL (no bird combinator for it)
+
+maxSubArray = maximum 
+            . scanl1 (bfork kite max (+))
