741f142daa45c8e9055c22366d34181f8fa852d9
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-13 17:35:54 -0400

    Update P0053_Max_Subarray_Sum.kt

--- P0053_Max_Subarray_Sum.kt
+++ P0053_Max_Subarray_Sum.kt
@@ -1,4 +1,5 @@
 // Problem Link: https://leetcode.com/problems/maximum-subarray/
+// Kotlin Playground: https://pl.kotl.in/6vqoX0ust
 
 fun maxSubArray(nums: List<Int>): Int {
     return nums.runningReduce{ a, b -> maxOf(a + b, b) }
