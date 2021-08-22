bda37165de0216cff3da2f397d31cb18d5720388
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-22 18:39:10 -0400

    Create 0255_Problem_1.cpp

---
+++ 0255_Problem_1.cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
+// Godbolt: https://godbolt.org/z/zK4zexfza
+
+// C++17
+int findGCD(std::vector<int> const& nums) {
+    auto const [a, b] = std::minmax_element(nums.cbegin(), nums.cend());
+    return std::gcd(*a, *b);
+}
+
+// C++20
+int findGCD(std::vector<int> const& nums) {
+    auto const r = std::ranges::minmax_element(nums);
+    return std::gcd(*r.min, *r.max);
+}
