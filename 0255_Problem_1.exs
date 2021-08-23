ea7b0ea27763f6201cb7cfccd3d4cd498112e9d4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-22 23:45:53 -0400

    Create 0255_Problem_1.exs

---
+++ 0255_Problem_1.exs
@@ -0,0 +1,3 @@
+# Problem Link: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
+
+def find_gcd(nums), do: Integer.gcd(Enum.min(nums), Enum.max(nums))
