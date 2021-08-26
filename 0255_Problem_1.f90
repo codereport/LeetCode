5de964be1f5333251c71a82382dd5a558772e3ef
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-08-26 16:28:44 -0400

    Create 0255_Problem_1.f90

---
+++ 0255_Problem_1.f90
@@ -0,0 +1,32 @@
+! Problem Link: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
+
+! gcd function take from: https://programming-idioms.org/idiom/74/compute-gcd/3741/fortran
+function gcd(m, n) result(answer)
+    implicit none
+    integer, intent(in)  :: m, n
+    integer              :: answer, irest, ifirst
+
+    ifirst = iabs(m)
+    answer = iabs(n)
+    if (answer.eq.0) then
+        answer = ifirst
+    else
+        do
+            irest = mod(ifirst, answer)
+            if (irest == 0)  exit
+            ifirst = answer
+            answer = irest
+        enddo
+        answer = iabs(answer)
+    endif
+end function gcd
+
+! LeetCode Solution
+function find_gcd(nums, numsSize) result(res)
+    implicit none
+    integer, intent(in) :: numsSize
+    integer, intent(in) :: nums(numsSize)
+    integer             :: res
+
+    res = gcd(minval(nums), maxval(nums))
+end function find_gcd
