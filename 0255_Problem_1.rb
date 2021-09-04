2bff1c4e47b8c1a255d403ce153ce929fa85aee2
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-09-04 19:31:37 -0400

    Update 0255_Problem_1.rb

--- 0255_Problem_1.rb
+++ 0255_Problem_1.rb
@@ -1,5 +1,16 @@
 # Problem Link: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
 
+# Solution 1 (original)
 def find_gcd(nums)
     return nums.max.gcd(nums.min)
+end
+
+# Solution 2 (omit return)
+def find_gcd(nums)
+    nums.max.gcd(nums.min)
+end
+
+# Solution 3 (use minmax)
+def find_gcd(nums)
+    nums.minmax.reduce(:gcd)
 end
