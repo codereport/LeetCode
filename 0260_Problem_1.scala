8e0ca4ad694e1bfa24a3af299f24c0cf9204c8f4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-09-27 08:02:35 -0400

    Update 0260_Problem_1.scala

--- 0260_Problem_1.scala
+++ 0260_Problem_1.scala
@@ -1,10 +1,9 @@
 // Problem Link: https://leetcode.com/contest/weekly-contest-260/problems/maximum-difference-between-increasing-elements/
 
-def maximumDifference(nums: Array[Int]): Int = {
+def maximumDifference(nums: Array[Int]): Int =
     nums.scanLeft(Int.MaxValue)(_ min _)
         .tail
         .zip(nums)
         .map(x => x._2 - x._1)
         .filter(_ != 0)
         .foldLeft(-1)(_ max _)
-}
