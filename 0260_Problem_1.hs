a77c9efae0c45e7fafaa7fe2fcab9e4447a2b0c3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-09-28 10:19:24 -0400

    Update 0260_Problem_1.hs

--- 0260_Problem_1.hs
+++ 0260_Problem_1.hs
@@ -1,5 +1,11 @@
 -- Problem Link: https://leetcode.com/contest/weekly-contest-260/problems/maximum-difference-between-increasing-elements/
 
+-- Original
 maximumDifference = foldl max (-1) 
                   . filter (/=0) 
                   . (zipWith (flip (-)) <*> scanr1 max)
+
+-- 
+maximumDifference = foldl max (-1) 
+                  . filter (>0) 
+                  . (zipWith (-) <*> scanl1 min)
