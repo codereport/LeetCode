b51b12cada04f5f5f0f779691a38c903efda255f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-10-05 11:23:33 -0400

    Update P0041_Find_Missing_Positive.cu

--- P0041_Find_Missing_Positive.cu
+++ P0041_Find_Missing_Positive.cu
@@ -1,11 +1,26 @@
 // Problem Link: https://leetcode.com/problems/first-missing-positive/
 // Godbolt Link: https://godbolt.org/z/ro4d5fb6h
 
+// Solution 1 (parition, sort & mismatch)
+// Godbolt Link: https://godbolt.org/z/ro4d5fb6h
+
 auto find_missing_positive(thrust::device_vector<int> vec) -> int {
     auto i = thrust::partition(vec.begin(), vec.end(), 
         [n = vec.size()] __device__ (auto e) { return 0 < e and e <= n; });
     thrust::sort(vec.begin(), i);
     return *thrust::mismatch(vec.cbegin(), 
+                             vec.cend(), 
+                             thrust::make_counting_iterator(1)).second;
+}
+
+// Solution 2 (scatter_if & mismatch)
+// Godbolt Link: https://godbolt.org/z/hPhas9TvY
+
+auto find_missing_positive(thrust::device_vector<int> vec) -> int {
+    auto s = thrust::device_vector<int>(vec.size() + 1, 0);
+    thrust::scatter_if(vec.begin(), vec.end(), vec.begin(), vec.begin(), s.begin(), 
+        [n = vec.size()] __device__ (auto e) { return 0 < e and e <= n; });
+    return *thrust::mismatch(vec.cbegin() + 1, 
                              vec.cend(), 
                              thrust::make_counting_iterator(1)).second;
 }
