b90d96348156129d3631b72dd0bb855fb0be99e0
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-10-05 11:26:18 -0400

    Update P0041_Find_Missing_Positive.cu

--- P0041_Find_Missing_Positive.cu
+++ P0041_Find_Missing_Positive.cu
@@ -14,13 +14,13 @@ auto find_missing_positive(thrust::device_vector<int> vec) -> int {
 }
 
 // Solution 2 (scatter_if & mismatch)
-// Godbolt Link: https://godbolt.org/z/hPhas9TvY
+// Godbolt Link: https://godbolt.org/z/K7rz5h19z
 
 auto find_missing_positive(thrust::device_vector<int> vec) -> int {
     auto s = thrust::device_vector<int>(vec.size() + 1, 0);
     thrust::scatter_if(vec.begin(), vec.end(), vec.begin(), vec.begin(), s.begin(), 
         [n = vec.size()] __device__ (auto e) { return 0 < e and e <= n; });
-    return *thrust::mismatch(vec.cbegin() + 1, 
-                             vec.cend(), 
+    return *thrust::mismatch(s.cbegin() + 1, 
+                             s.cend(), 
                              thrust::make_counting_iterator(1)).second;
 }
