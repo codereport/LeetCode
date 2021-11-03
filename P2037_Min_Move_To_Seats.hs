5bb0044d55a3ce0f2cf65e2d69d38e2f13100240
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-11-03 00:02:52 -0400

    Create P2037_Min_Move_To_Seats.hs

---
+++ P2037_Min_Move_To_Seats.hs
@@ -0,0 +1,12 @@
+--  Problem Link: https://leetcode.com/contest/biweekly-contest-63/problems/minimum-number-of-moves-to-seat-everyone/
+
+import Data.List (sort)
+import Data.Function (on)
+import Data.Composition (.:)
+
+minMoveToSeats = sum . map abs .: zipWith (-) `on` sort
+
+-- Tests
+minMoveToSeats [3,1,5] [2,7,4]     -- 4
+minMoveToSeats [4,1,5,9] [1,3,2,6] -- 7
+minMoveToSeats [2,2,6,6] [1,3,2,6] -- 4
