7ad97ed7d2016c219c9819a42407ee1d954def42
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-11-04 01:13:10 -0400

    Update P2037_Min_Move_To_Seats.py

--- P2037_Min_Move_To_Seats.py
+++ P2037_Min_Move_To_Seats.py
@@ -1,5 +1,15 @@
 # https://leetcode.com/contest/biweekly-contest-63/problems/minimum-number-of-moves-to-seat-everyone/
 
+# Solution 1
+
+def minMovesToSeat(self, a, b):
+    a.sort()
+    b.sort()
+    res = 0
+    for x, y in zip(a, b):
+        res += abs(x - y)
+    return res
+
 # Solution 2
 
 def minMovesToSeat(self, a, b):
