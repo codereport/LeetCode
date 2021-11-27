3a9f2e29ace24a6a5fea0ab96ed62f7ead0f0a48
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-11-27 13:39:12 -0500

    Update 0210_Problem_1.clj

--- 0210_Problem_1.clj
+++ 0210_Problem_1.clj
@@ -2,6 +2,15 @@
 ;; Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
 ;; Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 
+;; Original Solution
+(defn max-depth [s]
+  (->> s 
+       (re-seq #"[()]")
+       (map #(if (= % "(") 1 -1))
+       (reductions +)
+       (apply max)))
+
+;; Alternative solution
 (defn max-depth [s]
   (->> s
        (keep {\( 1, \) -1})
