470dde63a9c12dd97c6b93c64023d82db67b6b83
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2022-08-11 13:22:49 -0400

    Update 0299_Problem_1.py

--- 0299_Problem_1.py
+++ 0299_Problem_1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def check_matrix(grid):
     i = np.identity(len(grid), int)
-    return np.array_equal(np.minimum(grid, 1), np.maximum(i, np.flip(i, axis = 1)))
+    return np.array_equal(np.minimum(grid, 1), np.maximum(i, np.flip(i, axis=1)))
 
 check_matrix(np.matrix('2 0 0 1; 0 3 1 0; 0 5 2 0; 4 0 0 2')) # True
 check_matrix(np.matrix('5 7 0; 0 3 1; 0 5 0'))                # False
