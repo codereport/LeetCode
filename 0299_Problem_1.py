a1ad78b327c50a89842c9e520afa7bf3616fe157
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2022-09-16 21:52:59 -0400

    Update 0299_Problem_1.py

--- 0299_Problem_1.py
+++ 0299_Problem_1.py
@@ -9,9 +9,9 @@ def check_matrix(grid):
 
 # Without using np.identity
 def check_matrix(grid):
-   n = len(grid)
-   i = np.equal.outer(np.arange(n), np.arange(n))
-   return np.array_equal(np.minimum(grid, 1), np.maximum(i, np.flip(i, axis=1)))
+    n = len(grid)
+    i = np.equal.outer(np.arange(n), np.arange(n))
+    return np.array_equal(np.minimum(grid, 1), np.maximum(i, np.flip(i, axis=1)))
 
 check_matrix(np.matrix('2 0 0 1; 0 3 1 0; 0 5 2 0; 4 0 0 2')) # True
 check_matrix(np.matrix('5 7 0; 0 3 1; 0 5 0'))                # False
