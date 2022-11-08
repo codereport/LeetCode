c0e0f65eb9ad37068474da12df43108b8c98d6ec
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2022-11-07 22:47:34 -0500

    Update P1287_Majority_Element.py

--- P1287_Majority_Element.py
+++ P1287_Majority_Element.py
@@ -1,4 +1,4 @@
-# Problem Link: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/submissions/
+# Problem Link: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array
 
 def findSpecialInteger(self, arr: List[int]) -> int:
     return max(map(lambda t: t[::-1], Counter(arr).items()))[1]
