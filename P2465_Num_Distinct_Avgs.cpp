2dbebb64c4e13f774b001e99b19eb5cd894c3052
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2022-11-14 11:21:21 -0500

    Create P2465_Num_Distinct_Avgs.cpp

---
+++ P2465_Num_Distinct_Avgs.cpp
@@ -0,0 +1,9 @@
+// Problem Link: https://leetcode.com/contest/biweekly-contest-91/problems/number-of-distinct-averages/
+
+auto distinct_averages(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    std::unordered_set<int> sums;
+    for (int i = 0; i < nums.size() - 1 - i; i++)
+        sums.insert(nums[i] + nums[nums.size() - 1 - i]);
+    return sums.size();
+}
