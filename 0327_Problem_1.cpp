2a1ba85b35788a095a683dee01e5081b82ed494b
Conor Hoekstra <codereport@outlook.com>
2023-01-11 02:56:03 -0500

    Addtional C++ solution

--- 0327_Problem_1.cpp
+++ 0327_Problem_1.cpp
@@ -18,3 +18,23 @@ int maximumCount(vector<int>& nums) {
     auto neg = std::count_if(nums.begin(), nums.end(), [](auto e) { return e < 0; });
     return std::max(pos, neg);
 }
+
+// Solution 3 (C++20)
+
+int maximumCount(vector<int>& nums) {
+    auto pos = std::ranges::count_if(nums, [](auto e) { return e > 0; });
+    auto neg = std::ranges::count_if(nums, [](auto e) { return e < 0; });
+    return std::max(pos, neg);
+}
+
+// Solution 4
+// LeetCode Benchmark: 23 ms
+
+auto maximum_count(std::vector<int> nums) -> int {
+    auto const [pos, neg] = std::accumulate(
+        nums.cbegin(), nums.cend(), std::pair{0, 0}, [](auto p, auto e) {
+            auto const [pos, neg] = p;
+            return std::pair{pos + (e > 0), neg + (e < 0)};
+        });
+    return std::max(pos, neg);
+}
