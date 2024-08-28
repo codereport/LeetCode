ecb9a7d815e9f4ed5361fba59d3fe0f3ace8ffb9
Conor Hoekstra <codereport@outlook.com>
2024-08-28 10:51:36 -0400

    :sparkles: additional python solution

--- Problem 412P1/python.py
+++ Problem 412P1/python.py
@@ -1,3 +1,15 @@
+# Solution 1
+def getFinalState(nums, k, m):
+    for _ in range(k):
+        i, lo = 0, nums[0]
+        for j, e in enumerate(nums):
+            if e < lo:
+                i, lo = j, e
+        nums[i] *= m
+    return nums
+
+
+# Solution 2
 def getFinalState(nums, k, m):
     for _ in range(k):
         i = nums.index(min(nums))
