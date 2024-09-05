a3b38250c56a5d538b5013a2ccebafd7e005c358
Conor Hoekstra <codereport@outlook.com>
2024-09-04 21:18:17 -0400

    :recycle: 2nd c++ solution

--- Problem 412P1/cpp.cpp
+++ Problem 412P1/cpp.cpp
@@ -1,10 +1,18 @@
 #include <vector>
 #include <ranges>
 
+// Solution
 auto getFinalState(std::vector<int> nums, int k, int m) {
     while (k--) {
         auto i = std::ranges::min_element(nums);
         *i *= m;
     }
+    return nums;
+}
+
+// Small refactor
+auto getFinalState(std::vector<int> nums, int k, int m) {
+    while (k--)
+        *std::ranges::min_element(nums) *= m;
     return nums;
 }
